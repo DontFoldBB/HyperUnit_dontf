3597f8180f07c6ef4356a0a89281b16db2f9440e
DontFoldBB <DontFoldBB@users.noreply.github.com>
2026-06-03 15:05:06 +0500

    ЗАПУСТИТЬ.bat: ASCII-комментарий вместо кириллицы (фикс ''локальный is not recognized'' при chcp 65001)

--- ЗАПУСТИТЬ.bat
+++ ЗАПУСТИТЬ.bat
@@ -1,7 +1,7 @@
 @echo off
 chcp 65001 >nul
 cd /d "%~dp0"
-REM --- выбрать Python: локальный .venv -> общий venv из first_try -> системный ---
+REM --- pick Python: local .venv -> shared venv from first_try -> system ---
 set "PY=.venv\Scripts\python.exe"
 if not exist "%PY%" set "PY=..\first_try\hyperliquid_trade\.venv\Scripts\python.exe"
 if not exist "%PY%" set "PY=python"
